087cc182297627fa05e1a287f94a9422ebbce9c1
Joon Suh <joon.suh2003@gmail.com>
2020-09-05 15:32:26 +1000

    Resolve merge issues

--- objects/Enemies/TestAdvEnemy.gd
+++ objects/Enemies/TestAdvEnemy.gd
@@ -1,25 +1,26 @@
 extends "res://scripts/BasicEnemy.gd"
 
 func _ready():
-	walkTime = 0.001
+	walk_time = 1
 
-func chooseMove(root):
-	var validSpot = false
+func choose_move(root):
+	var valid_spot = false
 	var pos = (position - Vector2(32,32)) / 64
 	pos.x = floor(pos.x)
 	pos.y = floor(pos.y)
-	var closestPlayer = 0
-	var closestPosition = 0
+	var closest_player
+	var closest_position = 0
 	for p in root.players:
 		var distance = position.distance_squared_to(p.position)
-		if closestPosition == 0 || distance <= closestPosition:
-			closestPosition = distance
-			closestPlayer = p
-	var playerPos = (closestPlayer.position - Vector2(32,32)) / 64
-	var x = floor(playerPos.x) + sign(pos.x - playerPos.x)
-	var y = floor(playerPos.y) + sign(pos.y - playerPos.y)
+		if closest_position == 0 || distance <= closest_position:
+			closest_position = distance
+			closest_player = p
+	var player_pos = (closest_player.position - Vector2(32,32)) / 64
+	var x = floor(player_pos.x) + sign(pos.x - player_pos.x)
+	var y = floor(player_pos.y) + sign(pos.y - player_pos.y)
 	var path = root.astar.get_point_path(pos.y * (root.maxX + 1) + pos.x, y * (root.maxX + 1) + x)
-	if len(path) >= 2 && len(path) <= root.currentTurnTime / walkTime + 1 && root.isTileFree(x, y):
-		return ["move", path, true]
+	if len(path) >= 2 && len(path) <= root.current_turn_time / walk_time + 1 && root.is_tile_free(x, y):
+		return [CONSTS.ENEMY_ACTION.WALK, path, true]
 	root.createBubbleText("[center][shake rate=25 level=30]I'm lost :(", position, false)
-	return ['hate', null, true]
+	return [null, null, true]
+
